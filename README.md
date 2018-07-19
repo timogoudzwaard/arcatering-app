1d89bd0331f8e6e056c04e97872df0634d34fa14
timogoudzwaard <timo.goudzwaard@gmail.com>
2018-07-19 10:50:42 +0200

    Add firebase to project

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ The official Arcatering App (PWA)
 - ESLint (AirBnB config)
 - Firebase for deploy
 - Service Worker for PWA (Progressive Web Application)
+
+## Firebase config
+Create a new project in firebase, then add the firebase config in src/config.js (create a new file).
+
+**Example**
+```
+const firebaseConfig = {
+  apiKey: 'xxxx',
+  authDomain: 'xxxx.firebaseapp.com',
+  databaseURL: 'https://xxxx.firebaseio.com',
+  projectId: 'xxxx',
+  storageBucket: 'xxxx.appspot.com',
+  messagingSenderId: '12341234',
+};
+
+export default firebaseConfig;
+```
--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase';
 
 // Components
 import Box from './components/Box/Box';
 
-const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <Box />
-    </div>
-  </BrowserRouter>
-);
+// Config
+import firebaseConfig from './config';
+
+class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Box />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
