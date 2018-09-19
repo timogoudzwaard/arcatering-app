077f32bc919779d576fba5c3b53aa93813dfdb93
timogoudzwaard <timo.goudzwaard@gmail.com>
2018-09-19 12:55:16 +0200

    Add initial logic

--- src/components/Homepage/Groceries/ClaimMoney/ClaimMoney.js
+++ src/components/Homepage/Groceries/ClaimMoney/ClaimMoney.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ActionButton } from '../../../common';
 
-const ClaimMoney = () => (
-  <div>
-    <h4>
-      Hoe duur waren de boodschappen?
-    </h4>
-    <input />
-    <ActionButton>
-      Claim geld
-    </ActionButton>
-  </div>
-);
+class ClaimMoney extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { amount: '', IBAN: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // sendMessage = ({ amount, users, IBAN }) => {
+  //   const amountPerUser = amount / users;
+  // };
+
+  handleChange(event) {
+    this.setState({ amount: event.target.value });
+  }
+
+  render() {
+    const { amount, IBAN } = this.state;
+    return (
+      <div>
+        <p>
+          Hoe duur waren de boodschappen?
+        </p>
+        <input type="text" value={amount} onChange={this.handleChange} />
+        <p>
+          Op welke rekening moet het gestort worden?
+        </p>
+        <input type="text" value={IBAN} onChange={this.handleChange} />
+        <ActionButton>
+          Claim geld
+        </ActionButton>
+      </div>
+    );
+  }
+}
 
 export default ClaimMoney;
